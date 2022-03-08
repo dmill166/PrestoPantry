32dddecd2e92cc778481a7f184b552c5c2e77a68
Riley <rjstrong0101@yahoo.com>
2022-03-07 18:25:22 -0700

    Clean up returns and validation error handling

--- prestopantry_app/views/account_views/login_views.py
+++ prestopantry_app/views/account_views/login_views.py
@@ -3,13 +3,16 @@ from django.shortcuts import render, redirect
 from prestopantry_app.backends.user_auth import UserAuthBackend
 
 def login(request):
+    content = {}
     if request.method == 'POST':
       email = request.POST['email']
       password = request.POST['pwd']
+      # Email validation HERE 
+      # same validation that's used in signup (doesn't change functionality but shouldn't ping the database with an invalid email)
       user = UserAuthBackend.authenticate(request, email=email, password=password)
       if user:
         auth_login(request, user)
         return redirect('/')
       else:
-        return render(request, 'login.html', {'error': 'Invalid email or password'})
-    return render(request, 'login.html')
+        content['error'] = 'Invalid email or password'
+    return render(request, 'login.html', content)
--- prestopantry_app/views/account_views/signup_views.py
+++ prestopantry_app/views/account_views/signup_views.py
@@ -4,18 +4,21 @@ from prestopantry_app.models.users import User
 from prestopantry_app.backends.user_auth import UserAuthBackend
 
 def signup(request):
+    content = {}
     if request.method == 'POST':
       email = request.POST['email']
       username = request.POST['username']
       password = request.POST['pwd']
-      # Email, username(display name) and password validation HERE
+      # Email, username(display name) and password validation HERE (see examples below)
       if (UserAuthBackend.email_taken(email)):
-        return render(request, 'signup.html', {'error': 'Email already taken'})
+        content['error'] = 'Email already taken'
       if (UserAuthBackend.username_taken(username)):
-        return render(request, 'signup.html', {'error': 'Username already taken'})
-      user = User.objects.create_user(username, email, password)
-      user.save()
-      login(request, user)
-      return render(request, 'signup.html')
-    else:
-        return render(request, 'signup.html')
+        content['error'] = 'Username already taken'
+      if len(password) < 8:
+        content['error'] = 'Password must be at least 8 characters long'
+      # Create and log user in if no validation errors
+      if content == {}:
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        login(request, user)
+    return render(request, 'signup.html', content)
