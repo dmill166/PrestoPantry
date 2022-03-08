1dec55d9c5424999151264f3f595b0f9ca12735b
Riley <rjstrong0101@yahoo.com>
2022-03-07 18:33:22 -0700

    Change email not be nullable

--- prestopantry_app/models/users.py
+++ prestopantry_app/models/users.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # custom user class that extends the default user class
 class User(AbstractUser):
-    email = models.EmailField(unique=True, null= True, max_length=254)
+    email = models.EmailField(unique=True, max_length=254)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     REQUIRED_FIELDS = ['first_name', 'last_name', 'email']  
