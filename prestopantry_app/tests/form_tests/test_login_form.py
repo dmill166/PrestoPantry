6e8100f2a0936d5bceeea1685d6e592465533d99
Riley Strong <56652106+riley-strong@users.noreply.github.com>
2022-03-08 21:43:34 -0700

    add missed case to LoginFormTests

--- prestopantry_app/tests/form_tests/test_login_form.py
+++ prestopantry_app/tests/form_tests/test_login_form.py
@@ -9,14 +9,19 @@ class LoginFormTests(TestCase):
         form_invalid_email = LoginForm({'email': 'test1@gma', 'password': 'test_password'})
         self.assertFalse(form_invalid_email.is_valid())
 
-        # valid email and password (user exists)
+        # valid email and password 
         User.objects.create_user(username='test_user', email="test1@gmail.com", password="test_password")
         form_valid = LoginForm(data = {'email': 'test1@gmail.com', 'password': 'test_password'})
 
-        UserModel = get_user_model()    
         self.assertTrue(form_valid.is_valid())
+        UserModel = get_user_model()    
         self.assertEquals(form_valid.user_cache, UserModel.objects.get(email='test1@gmail.com'))
 
-        # Invalid password (user exists)
+        # Incorrect email
+        form_incorrect_email = LoginForm(data = {'email': 'test@gmail.com', 'password': 'test_password'})
+        self.assertFalse(form_incorrect_email.is_valid())
+        
+        # Incorrect password 
         form_incorrect_password = LoginForm(data = {'email': 'test1@gmail.com', 'password': 'password'})
         self.assertFalse(form_incorrect_password.is_valid())
+        
