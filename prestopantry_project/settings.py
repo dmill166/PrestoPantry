d29bd02168fd711165531617381089d71f1fd5ec
Adam Wojdyla <awojdyla@gmail.com>
2022-03-15 16:01:34 -0600

    creates pantry search page

--- prestopantry_project/settings.py
+++ prestopantry_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-
+SPOON_API_KEY = os.environ["SPOON_API_KEY"]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
