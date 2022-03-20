4c5a6d7ac859ffe50331f9b7cd4581e110aaed1e
Riley <rjstrong0101@yahoo.com>
2022-03-19 18:58:20 -0600

    edit user info

---
+++ prestopantry_app/forms/edit_account_forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.forms import UserChangeForm
+from prestopantry_app.models.users import User
+
+
+class EditUsernameForm(UserChangeForm):
+    password = None
+    username = forms.CharField(label="Display Name", max_length=50)
+
+    class Meta:
+        model = User
+        fields = ['username']
+
+
+class EditEmailForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+
+class EditNameForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
--- prestopantry_app/urls.py
+++ prestopantry_app/urls.py
@@ -2,14 +2,15 @@
 from unicodedata import name
 from django.urls import path
 
-from .views.account_views import signup_views, login_views
+from .views.account_views import login, signup, edit_account
 from .views import main_views
 
 urlpatterns = [
     path('', main_views.community, name='home'),
     path('landing-page/', main_views.landing_page, name='landing_page'),
-    path('signup/', signup_views.signup, name='signup'),
-    path('login/', login_views.login, name='login'),
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
+    path('account/', edit_account, name='account'),
     path('logout/', main_views.logout_view, name='logout'),
     path('about/', main_views.about, name='about'),
 ]
