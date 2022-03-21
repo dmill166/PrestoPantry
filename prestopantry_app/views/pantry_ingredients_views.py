3cbb83e1d7bed4913e76a0bcfd562cfe11b59fb5
Adam Wojdyla <awojdyla@gmail.com>
2022-03-20 23:01:48 -0600

    unit tests for views, backend-api

--- prestopantry_app/views/pantry_ingredients_views.py
+++ prestopantry_app/views/pantry_ingredients_views.py
@@ -1,6 +1,7 @@
 from prestopantry_app.backends.spoonacular_api import JsonScraper
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+import ipdb
 
 @login_required(login_url='login')
 def search_by_ingredient(request):
@@ -13,24 +14,29 @@ def search_by_ingredient(request):
 
         response = JsonScraper.ingredient_request("POST", data=str(payload))
         ingredient_json = response.json()
-        context = JsonScraper.harvest_ingredients(ingredient_json)
+        if response.status_code == 200 and ingredient_json != []:
+          context = JsonScraper.harvest_ingredients(ingredient_json)
       
-        return render(request, 'pantry_page.html', {'context': context})
+          return render(request, 'pantry_ingredients_page.html', {'context': context})
 
-      if request.method == 'POST' and 'ingredient_per_recipe_button' in request.POST:
+      elif request.method == 'POST' and 'ingredient_per_recipe_button' in request.POST:
     
         # Whether to maximize used ingredients ranking:1 or minimize missing ingredients ranking:2 first.
         payload = {
           'ingredients':[request.POST['ingredients']],
           'number':5,
           'ignorePantry':'false',
-          'ranking':1
+          'ranking':2
           }
 
         response = JsonScraper.recipe_request("GET", params=payload)
         recipe_json = response.json()
-        context = JsonScraper.harvest_recipe_per_ingredients(recipe_json)
+        if response.status_code == 200 and recipe_json != []:
 
-        return render(request, 'pantry_page.html', {'recipe_context': context})
-      else:
-        return render(request, 'pantry_page.html', {})
+          # ipdb.set_trace()
+
+          context = JsonScraper.harvest_recipe_per_ingredients(recipe_json)
+
+          return render(request, 'pantry_ingredients_page.html', {'recipe_context': context})
+      
+      return render(request, 'pantry_ingredients_page.html', {})
