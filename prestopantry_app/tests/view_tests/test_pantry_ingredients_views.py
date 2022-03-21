a3e07484f9c21f3695a112c76676da8fb1c6ec8b
Adam Wojdyla <awojdyla@gmail.com>
2022-03-20 23:01:48 -0600

    unit tests for views, backend-api

---
+++ prestopantry_app/tests/view_tests/test_pantry_ingredients_views.py
@@ -0,0 +1,37 @@
+from multiprocessing.connection import Client
+from django.test import TestCase
+from django.urls import reverse
+from prestopantry_app.models.users import User
+
+
+# context = {
+#     'image': 'garlic-powder.png', 
+#     'ingredient_info': [('Garlic Powder', 872421, '842441131321')], 
+#     'ingredient_name': 'garlic powder'
+#     }
+
+
+class PantryIngredientsViewTest(TestCase):
+    def setUp(self):
+        # self.client = Client()
+        self.user = User.objects.create_user('goldmember','drevil@gmail.com','ilovegold')
+
+    def test_pantry_ingredients_view(self):
+        # login
+        self.client.login(username='goldmember', email= 'drevil@gmail.com', password='ilovegold')
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        # go to pantry search page
+        response = self.client.get(reverse('pantry-ingredients'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pantry_ingredients_page.html')
+        self.assertContains(response, '<h2>Ingredient Search Central</h2>')
+
+        # test search
+        data = {'q': '88'}
+        response = self.client.get(reverse('pantry-ingredients'), data)
+        self.assertContains(response, 'No Searches found...')
+
+        # response = self.client.post('/pantry-ingredients/', context, content_type='application/json')
+        # self.assertEqual(response.status_code, 200 )
+    
