d466434b20c1322fd9b01a12711084d356b672d5
Dakota Miller <dmill166@msudenver.edu>
2022-03-27 21:39:16 -0600

    Attempt for GitGuardian GGShield CLI

--- prestopantry_project/settings.py
+++ prestopantry_project/settings.py
@@ -180,3 +180,9 @@ STATICFILES_DIRS = [BASE_DIR/'prestopantry_app/static']
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#~/.bash_profile
+GITGUARDIAN_API_KEY=os.environ['GITGUARDIAN_API_KEY']
+
+#/etc/environment
+GITGUARDIAN_API_KEY=os.environ['GITGUARDIAN_API_KEY']
