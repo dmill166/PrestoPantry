681eaa4747dcfede8b63c67cee87952009e4c86f
Riley <rjstrong0101@yahoo.com>
2022-04-02 16:33:38 -0600

    Add google oauth for signup/login

--- prestopantry_app/models/users.py
+++ prestopantry_app/models/users.py
@@ -9,3 +9,6 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=50, validators=[RegexValidator(regex='^[a-zA-Z]+$', message='First name must be letters only.')])
     last_name = models.CharField(max_length=50, validators=[RegexValidator(regex='^[a-zA-Z]+$', message='Last name must be letters only.')])
     REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
+
+    def oauth_user(self):
+        return not self.has_usable_password()
