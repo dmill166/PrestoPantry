8d4ec79fd4bf42280cfe525430fa70e6973c073b
Hector Cruz <hcruz5@Hectors-MacBook-Pro.local>
2022-04-02 12:34:40 -0600

    Added upc to model and tested

--- prestopantry_app/models/user_ingredients.py
+++ prestopantry_app/models/user_ingredients.py
@@ -6,3 +6,4 @@ class UserIngredient(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     ingredient_id = models.IntegerField()
     ingredient_name = models.CharField(max_length=300)
+    upc = models.BigIntegerField()
