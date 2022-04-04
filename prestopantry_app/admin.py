32929ae09570a50247f7a8f330c62e43e2182037
Riley <rjstrong0101@yahoo.com>
2022-04-04 17:06:08 -0600

    Allow multiple products to be added on same page and display verification message

--- prestopantry_app/admin.py
+++ prestopantry_app/admin.py
@@ -5,6 +5,17 @@ from prestopantry_app.models.developers import Developer
 from prestopantry_app.models.users import User
 from prestopantry_app.models.user_ingredients import UserIngredient
 
+
+class UserIngredientInline(admin.TabularInline):
+    model = UserIngredient
+    extra = 0
+    readonly_fields = ('ingredient_name', 'ingredient_id', 'upc')
+  
+
+class UserAdmin(UserAdmin):
+    inlines = (UserIngredientInline,)
+
+
 # Register your models here.
 admin.site.register(User, UserAdmin)
 admin.site.register(Developer)
--- prestopantry_app/views/pantry_ingredients_views.py
+++ prestopantry_app/views/pantry_ingredients_views.py
@@ -1,30 +1,29 @@
-from ast import Add
 from prestopantry_app.backends.spoonacular_api import SpoonacularAPI
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from prestopantry_app.models.user_ingredients import UserIngredient
 from prestopantry_app.models.user_ingredients import UserIngredient
 
 
 @login_required(login_url='login')
 def search_by_ingredient(request):
-      json_scraper = SpoonacularAPI
-      if request.method == 'POST' and 'ingredient_button' in request.POST:
+    json_scraper = SpoonacularAPI
+    if request.method == 'POST' and 'ingredient_button' in request.POST:
 
-        payload = { 
-          'ingredients': [request.POST['ingredient_name']], 
-          'servings': 1 
+        payload = {
+          'ingredients': [request.POST['ingredient_name']],
+          'servings': 1
           }
 
         response = json_scraper.ingredient_request("POST", data=str(payload))
         ingredient_json = response.json()
         if response.status_code == 200 and ingredient_json != []:
-          context = json_scraper.harvest_ingredients(ingredient_json)
-      
-          return render(request, 'pantry_ingredients_page.html', {'context': context})
+            context = json_scraper.harvest_ingredients(ingredient_json)
+            request.session['ingredient_search_results'] = context
 
-      elif request.method == 'POST' and 'ingredient_per_recipe_button' in request.POST:
-    
+            return render(request, 'pantry_ingredients_page.html', context)
+
+    elif request.method == 'POST' and 'ingredient_per_recipe_button' in request.POST:
+
         # Whether to maximize used ingredients ranking:1 or minimize missing ingredients ranking:2 first.
         payload = {
           'ingredients':[request.POST['ingredients']],
@@ -37,15 +36,23 @@ def search_by_ingredient(request):
         recipe_json = response.json()
         if response.status_code == 200 and recipe_json != []:
 
-          context = json_scraper.harvest_recipe_per_ingredients(recipe_json)
+            context = json_scraper.harvest_recipe_per_ingredients(recipe_json)
 
-          return render(request, 'pantry_ingredients_page.html', {'recipe_context': context})
+            return render(request, 'pantry_ingredients_page.html', {'recipe_context': context})
 
-      elif request.method == 'POST' and 'add_ingredient_button' in request.POST:
+    elif request.method == 'POST' and 'add_ingredient_button' in request.POST:
         obj = UserIngredient.objects
         ingredient = obj.create(user=request.user, ingredient_id=int(request.POST['ingredient_id']),
-        ingredient_name=request.POST['ingredient_name'], upc=int(request.POST['upc']))
-        
-        return render(request, 'pantry_ingredients_page.html')
-      
-      return render(request, 'pantry_ingredients_page.html', {})
+                                ingredient_name=request.POST['ingredient_name'], upc=int(request.POST['upc']))
+        session_ingredient_info = request.session['ingredient_search_results']['ingredient_info']
+        for i in session_ingredient_info:
+            if i[1] == ingredient.ingredient_id:
+                i.append(True)
+                new_ingredient_search_results = request.session['ingredient_search_results']
+                new_ingredient_search_results['ingredient_info'] = session_ingredient_info
+                request.session['ingredient_search_results'] = new_ingredient_search_results
+                break
+
+        return render(request, 'pantry_ingredients_page.html', request.session['ingredient_search_results'])
+
+    return render(request, 'pantry_ingredients_page.html', {})
