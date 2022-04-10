92c1fb4822df2eb4281bef6038b7590573001858
Adam Wojdyla <awojdyla@gmail.com>
2022-04-10 15:56:12 -0600

    added styling to several html pages

--- prestopantry_project/settings.py
+++ prestopantry_project/settings.py
@@ -48,7 +48,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'social_django',
     'storages',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
