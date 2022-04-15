e93464c8846ba6ea520d38809b989d559631f766
Hector Cruz <hcruz5@MacBook-Pro.local>
2022-04-14 22:35:08 -0600

    fixed conflicts

--- prestopantry_app/tests/view_tests/test_pantry_ingredients_views.py
+++ prestopantry_app/tests/view_tests/test_pantry_ingredients_views.py
@@ -41,3 +41,25 @@ class PantryIngredientsViewTest(TestCase):
         except UserIngredient.DoesNotExist:
             self.fail("Ingredient failed to save." + str(data))
         self.assertTrue(self.client.session['ingredient_search_results']['ingredient_info'][0][3])
+
+    def test_my_pantry_ingredients_view(self):
+        response = self.client.get(reverse('pantry'))
+        self.assertRedirects(response, '/login/?next=/pantry/')
+         # login
+        self.client.force_login(self.user)
+        # go to pantry search page
+        response = self.client.get(reverse('pantry'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pantry.html')
+
+        # test search for empty pantry
+        response = self.client.get(reverse('pantry'))
+        self.assertContains(response, 'Nothing in pantry')
+
+        # test search for pantry with ingredients
+        ingredient = UserIngredient.objects.create(ingredient_name='Torkelson Cheese Co. Brick Cheese Wisconsin',
+                                                  ingredient_id='406181', user=self.user, upc=123344564378)
+        self.client.force_login(self.user)
+        response = self.client.get('/pantry/')
+        self.assertEqual(response.context['ingred'].all().get(), ingredient)
+
